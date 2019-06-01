8225192e62f87d25104567323e9b5dac6c7f7de1
sth144 <sthinds144@gmail.com>
2019-06-01 17:33:30 -0500

    fixed API_URL

--- src/routes/urls.ts
+++ src/routes/urls.ts
@@ -2,7 +2,7 @@
  * set API url dynamically
  */
 let _URL = "http://localhost:8080";
-if (process.env.GOOGLE_CLOUD_PROJECT == "hindss-assign4") {
-    _URL = "https://hindss-assign4.appspot.com"
+if (process.env.GOOGLE_CLOUD_PROJECT == "hindss-final") {
+    _URL = "https://hindss-final.appspot.com"
 }
 export const API_URL = _URL;
